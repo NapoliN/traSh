daa1d26934a5bc9247fbd1ed81c68d1c24c221ac
Yuzuki Sakashita <sakashita.y.aa@m.titech.ac.jp>
2024-05-10 02:10:04 +0900

    add mypy

--- p.py
+++
@@ -1,5 +0,0 @@
-import re
-
-r = re.compile(r"[亜-熙]")
-m = r.match("宇")
-print(m)
--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from src.shell import ShellEmulator
 
 if __name__ == '__main__':
-    ShellEmulator().cmdloop()    
+    ShellEmulator().cmdloop()
